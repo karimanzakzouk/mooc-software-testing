d0be55e8569cfbda36c3daf3a8736965a9c15f97
kariman zakzouk <karimanzakzouk@gmail.com>
2020-12-06 20:54:30 +0200

    Add ghappy test and  solve a bug in ghappy example

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -1,18 +1,34 @@
 package tudelft.ghappy;
+import java.util.Scanner;
+import java.lang.String;
 
 public class GHappy {
 
-    public boolean gHappy(String str) {
+    public boolean gHappy(String str)
+    {
         assert str!=null;
-        for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
-                if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
+
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(str.charAt(i) == 'g' && str.length() > 2 )
+            {
+                if (i >= 0 && str.charAt(i-1) == 'g')
+                {
+                    continue;
+                }
+                if (i+1 < str.length() && str.charAt(i+1) == 'g')
+                {
+                    continue;
+                }
+                return false;
+            }
+            else if (str.charAt(i) == 'g' && str.length() == 1 )
+            {
                 return false;
             }
         }
 
         return true;
-
     }
+
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,26 @@
 package tudelft.ghappy;
 
-public class GHappyTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class GHappyTest
+{
+    @ParameterizedTest(name = "{0}: {1} = {2}")
+    @CsvSource({
+            "'test1','xxggxx',true",
+            "'test2','xxgxx',false",
+            "'test3','xxggyygxx',false",
+            "'test4','g', false",
+            "'test5','gg', true",
+            "'test6','xxgggxygg', true",
+            "'test7','yyygggxyy', true",
+            "'test8','', true"
+    })
+
+    public void testAlgorithm(String partition, String str ,  boolean expectedResult)
+    {
+        boolean result = new GHappy().gHappy(str);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
